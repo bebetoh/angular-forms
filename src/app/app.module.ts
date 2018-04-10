266cb17f05c6ccf1f3ffb01135a43a3929ccbeee
bebetoh <bebetohlf@gmail.com>
2018-04-10 00:07:45 -0300

    https://angular.io/guide/form-validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
+import { ForbiddenValidatorDirective } from './shared/forbidden-name.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroFormComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroFormComponent,
+    ForbiddenValidatorDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/forbidden-name.directive.ts
+++ src/app/shared/forbidden-name.directive.ts
@@ -1,8 +1,19 @@
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+        const forbidden = nameRe.test(control.value);
+        return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+    };
+}
+
 @Directive({
     selector: '[appForbiddenName]',
     providers: [{ provide: NG_VALIDATORS, useExisting: ForbiddenValidatorDirective, multi: true }]
 })
 export class ForbiddenValidatorDirective implements Validator {
+    // tslint:disable-next-line:no-input-rename
     @Input('appForbiddenName') forbiddenName: string;
 
     validate(control: AbstractControl): { [key: string]: any } {
